c353236732b5bb50e019490759034fa36e91543e
Nicolaswg <nigonbol123@gmail.com>
2022-09-02 21:58:57 -0400

    Implement styled component changes

--- packages/starter-widget/src/MyWidget.js
+++ packages/starter-widget/src/MyWidget.js
@@ -1,45 +1,44 @@
 import React, { useRef, useEffect, useState } from "react";
 import styled from "styled-components";
 import { usePrifina, Op } from "@prifina/hooks";
-import {ActivitySummary} from "@dynamic-data/oura-data/mockups"
+import { ActivitySummary } from "@dynamic-data/oura-data/mockups";
 import { Container } from "./components/Container";
-import Navbar from "./components/Navbar/Navbar";
-
+import Header from "./components/Header/Header";
+import Qoutes from "./components/Qoutes/Qoutes";
 
 // unique appID for the app....
 const appID = "1u3f465t4cNSWYiyKFVwBG";
-const ouraData = {...ActivitySummary}
-console.log(ouraData)
+const ouraData = { ...ActivitySummary };
+console.log(ouraData);
 
 const MyWidget = () => {
   // init hook and get provider api services...
-  const [user, setUser] = useState({})
+  const [user, setUser] = useState({});
 
   const requestRandomUser = async () => {
-    const request = await fetch('https://randomuser.me/api/');
-    const data = await request.json()
-    setUser(data.results[0])
-  }
+    const request = await fetch("https://randomuser.me/api/");
+    const data = await request.json();
+    setUser(data.results[0]);
+  };
 
   useEffect(() => {
-    requestRandomUser()
-  }, [])
-const Wrapper = styled.section`
-  padding: 0.5rem 0.2rem;
-  border-radius: 5px;
-  background: papayawhip;
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-`;
+    requestRandomUser();
+  }, []);
+  const Wrapper = styled.section`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+  `;
   return (
-    <Container variant="small" style={{ border: "1px solid blue" }}>
+    <Container variant="small" style={{ border: "1px solid #ee5522" }}>
       <Wrapper>
-      <Navbar ouraData={{
-          icon: '',
-          userName: 'Nicolas',
-          date: ouraData.summary_date,
-        }} userData={user}/>
+        <Header
+          ouraData={{
+            date: ouraData.summary_date,
+          }}
+          userData={user}
+        />
+        <Qoutes />
       </Wrapper>
     </Container>
   );
