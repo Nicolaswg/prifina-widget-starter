2824cdc0b7fea735b3c799cf06c320320a84aabd
Nicolaswg <nigonbol123@gmail.com>
2022-09-06 14:51:26 -0400

    Import and implement footer component

--- packages/starter-widget/src/MyWidget.js
+++ packages/starter-widget/src/MyWidget.js
@@ -5,6 +5,7 @@ import { ActivitySummary } from "@prifina/oura-data/mockups";
 import { Container } from "./components/Container";
 import Header from "./components/Header/Header";
 import Qoutes from "./components/Qoutes/Qoutes";
+import Footer from "./components/Footer/Footer";
 
 // unique appID for the app....
 const appID = "1u3f465t4cNSWYiyKFVwBG";
@@ -14,14 +15,14 @@ const ouraData = { ...ActivitySummary };
 const MyWidget = () => {
   // init hook and get provider api services...
   const [user, setUser] = useState({});
-  const {summary_date, score, rest, non_wear} = ouraData[0];
+  const {summary_date, score, rest, non_wear, cal_total} = ouraData[0];
   const requestRandomUser = async () => {
     const request = await fetch("https://randomuser.me/api/");
     const data = await request.json();
-    setUser(data.results[0]);1
+    setUser(data.results[0]);
   };
 
-  useEffect(() => {
+  useEffect( async () => {
     requestRandomUser();
   }, []);
   const Wrapper = styled.section`
@@ -30,9 +31,8 @@ const MyWidget = () => {
     width: 100%;
   `;
 
-  console.log(ouraData)
   return (
-    <Container variant="small" style={{ border: "1px solid #ee5522" }}>
+    <Container variant="small" style={{ border: "0.8px solid #ee5522" }}>
       <Wrapper>
         <Header
           ouraData={{
@@ -40,7 +40,8 @@ const MyWidget = () => {
           }}
           userData={user}
         />
-        <Qoutes score={score} rest={rest} no_ring={non_wear}/>
+        <Qoutes score={score} rest={rest} />
+        <Footer score={score} rest={rest} non_wear={non_wear} cal_total={cal_total}/>
       </Wrapper>
     </Container>
   );
