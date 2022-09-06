185aae81ba125e187d46daa7606355828773f07e
Nicolaswg <nigonbol123@gmail.com>
2022-09-05 20:41:33 -0400

    Implement qoutes rendering component (depending on some user Oura data values)

--- packages/starter-widget/src/MyWidget.js
+++ packages/starter-widget/src/MyWidget.js
@@ -1,7 +1,7 @@
 import React, { useRef, useEffect, useState } from "react";
 import styled from "styled-components";
 import { usePrifina, Op } from "@prifina/hooks";
-// import { ActivitySummary } from "@prifina/oura-data/mockups";
+import { ActivitySummary } from "@prifina/oura-data/mockups";
 import { Container } from "./components/Container";
 import Header from "./components/Header/Header";
 import Qoutes from "./components/Qoutes/Qoutes";
@@ -14,11 +14,11 @@ const ouraData = { ...ActivitySummary };
 const MyWidget = () => {
   // init hook and get provider api services...
   const [user, setUser] = useState({});
-
+  const {summary_date, score, rest, non_wear} = ouraData[0];
   const requestRandomUser = async () => {
     const request = await fetch("https://randomuser.me/api/");
     const data = await request.json();
-    setUser(data.results[0]);
+    setUser(data.results[0]);1
   };
 
   useEffect(() => {
@@ -29,16 +29,18 @@ const MyWidget = () => {
     flex-direction: column;
     width: 100%;
   `;
+
+  console.log(ouraData)
   return (
     <Container variant="small" style={{ border: "1px solid #ee5522" }}>
       <Wrapper>
         <Header
           ouraData={{
-            date: ouraData[0].summary_date,
+            date: summary_date,
           }}
           userData={user}
         />
-        <Qoutes />
+        <Qoutes score={score} rest={rest} no_ring={non_wear}/>
       </Wrapper>
     </Container>
   );
--- packages/starter-widget/src/components/Qoutes/Qoutes.js
+++ packages/starter-widget/src/components/Qoutes/Qoutes.js
@@ -1,9 +1,64 @@
-import React from 'react'
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import styled from "styled-components";
 
-const Qoutes = () => {
+const Qoutes = (props) => {
+  const {score, rest, no_ring} = props
+  const [qoutes, setQoutes] = useState({});
+
+  const requestQoutes = async (categorie) => {
+    const request = await fetch(
+      `https://api.api-ninjas.com/v1/quotes?category=${categorie}`,
+      {
+        headers: {
+          "X-Api-Key": "+j4RJ4BhcP9Xl6rFXrhGLQ==sjjL1haSVIXcqWrp",
+        },
+      }
+    );
+    const data = await request.json();
+    setQoutes(data[0]);
+  };
+
+  useEffect(async () => {
+    if(score < 71 || rest < 480) {
+      requestQoutes('success');
+    } else {
+      requestQoutes('fitness')
+    }
+  }, []);
+
+  const QoutesWrapper = styled.div`
+    width: 100%;
+    height: 160px;
+    display: flex;
+    background: red;
+    justify-content: center;
+    align-item: center;
+    padding: 0.4rem 0.8rem;
+    text-align: center;
+
+    p {
+      height: max-content;
+      text-align: center;
+      color: #fff;
+      background: #ee5522;
+      -webkit-box-shadow: 4px 6px 10px -1px rgba(0, 0, 0, 0.55);
+      -moz-box-shadow: 4px 6px 10px -1px rgba(0, 0, 0, 0.55);
+      box-shadow: 4px 6px 10px -1px rgba(0, 0, 0, 0.55);
+      padding: 0.3rem;
+      font-size: 0.8rem;
+      border-radius: 10px;
+      font-weight: 400;
+    }
+  `;
   return (
-    <div>Qoutes</div>
-  )
-}
+    <QoutesWrapper>
+      <p>
+        "{qoutes.quote}" .<small>{qoutes.author}</small>
+      </p>
+    </QoutesWrapper>
+  );
+};
 
-export default Qoutes
+export default Qoutes;
